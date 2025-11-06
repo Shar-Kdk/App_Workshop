2603fec6480d2f1e0aef5a6c8e41b7e43bd420ad
nitro <nitro@DESKTOP-94NIMFQ>
2025-11-06 21:23:24 +0545

    Add project files.

---
+++ Workshop2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Workshop2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Desc();
+        }
+
+        public static void Desc()
+        {
+            String userName = "Shar";
+            int luckyNumber = 2;
+            Console.WriteLine($"Hello {userName}!, Your lucky number is {luckyNumber}.");
+
+        }
+
+
+    }    
+}
